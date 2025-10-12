ee55840c716dfae1b1c69de129ab478dadd6a8dc
zanellaornela <zanellaornela1@gmail.com>
2025-10-12 12:55:32 -0300

    Update useIntersectionObserver hook to specify ref type as HTMLDivElement

--- src/app/hooks/useIntersectionObserver.ts
+++ src/app/hooks/useIntersectionObserver.ts
@@ -5,7 +5,7 @@ import { useEffect, useRef, useState } from 'react';
 export const useIntersectionObserver = (options = {}) => {
   const [isIntersecting, setIsIntersecting] = useState(false);
   const [hasIntersected, setHasIntersected] = useState(false);
-  const ref = useRef<HTMLElement>(null);
+  const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const element = ref.current;
